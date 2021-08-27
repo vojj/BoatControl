fe84e722973842e7db68f57b7acbb8db66438f06
vojj <danielw@gmx.eu>
2021-08-27 17:02:17 +0200

    add sccale for motor speed

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ from classes.class_esc_gpio import esc_gpio
 from classes.class_mqtt import mqtt_client
 
 #Init area
-motor1 = esc_gpio(0,100,18,50) # PWM0
-motor2 = esc_gpio(0,100,33,50) # PWM1
+motor1 = esc_gpio(0,100,18,50) # GPIO18, PWM0
+motor2 = esc_gpio(0,100,13,50) # GPIO18, PWM1
 
 #get and set information via mqtt to homebridge
 def SetSpeedMotor1(value):
@@ -103,30 +103,92 @@ title.pack(fill=tk.X, side=tk.TOP,expand=True)
 #HMI Motor1
 label = ttk.Label(text="--Motor1--", master = frameMotor1)
 label.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+#scale
+scaleSpeedMotor1 = 0
+def setSpeedMotor1(value):
+    global scaleSpeedMotor1
+    if(scaleSpeedMotor1 != int(float(value))):
+        motor1.forward(int(float(value)))
+        scaleSpeedMotor1 = int(float(value))
+
+scaleMotor1 = ttk.Scale(command = setSpeedMotor1, master = frameMotor1, from_ = 0, to = 100, orient=tk.HORIZONTAL )
+scaleMotor1.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+def update_speedmotor1():
+    global scaleSpeedMotor1
+    value = motor1.getSpeed()
+    if(scaleSpeedMotor1 != motor1.getSpeed()):
+        scaleMotor1.set(scaleSpeedMotor1)
+    window.after(300,update_speedmotor1)
+
+update_speedmotor1()
+#scale
+
 cmdSpeedUp = partial(motor1.speedUp, 1)
 button = ttk.Button(text="SpeedUp", master = frameMotor1, command = cmdSpeedUp)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
 cmdSpeedDown = partial(motor1.speedDown, 1)
 button = ttk.Button(text="SpeedDown", master = frameMotor1, command = cmdSpeedDown)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+button = ttk.Button(text="Arm", master = frameMotor1, command = motor1.arm)
+button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+button = ttk.Button(text="Stop", master = frameMotor1, command = motor1.SoftStop)
+button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
 label = ttk.Label(text="--Calibration--", master = frameMotor1)
 label.pack(fill=tk.X, side=tk.TOP,expand=True)
+
 button = ttk.Button(text="Step One", master = frameMotor1, command = motor1.calibrate_step1)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
 button = ttk.Button(text="Step Two", master = frameMotor1, command = motor1.calibrate_step2)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
 button = ttk.Button(text="Step Three", master = frameMotor1, command = motor1.calibrate_step3)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
 
 #HMI Motor2
 label = ttk.Label(text="--Motor2--", master = frameMotor2)
 label.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+#Scale
+scaleSpeedMotor2 = 0
+def setSpeedMotor2(value):
+    global scaleSpeedMotor2
+    if(scaleSpeedMotor2 != int(float(value))):
+        motor2.forward(int(float(value)))
+        scaleSpeedMotor2 = int(float(value))
+
+scaleMotor2 = ttk.Scale(command = setSpeedMotor2, master = frameMotor2, from_ = 0, to = 100, orient=tk.HORIZONTAL )
+scaleMotor2.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+def update_speedmotor2():
+    global scaleSpeedMotor2
+    value = motor2.getSpeed()
+    if(scaleSpeedMotor2 != value): 
+        scaleMotor2.set(scaleSpeedMotor2)
+    scaleMotor2.after(300,update_speedmotor2)
+
+update_speedmotor2()
+#Scale
+
 cmdSpeedUp = partial(motor2.speedUp, 1)
 button = ttk.Button(text="SpeedUp", master = frameMotor2, command = cmdSpeedUp)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
 cmdSpeedDown = partial(motor2.speedDown, 1)
 button = ttk.Button(text="SpeedDown", master = frameMotor2, command = cmdSpeedDown)
 button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+button = ttk.Button(text="Arm", master = frameMotor2, command = motor2.arm)
+button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
+button = ttk.Button(text="Stop", master = frameMotor2, command = motor2.SoftStop)
+button.pack(fill=tk.X, side=tk.TOP,expand=True)
+
 label = ttk.Label(text="--Calibration--", master = frameMotor2)
 label.pack(fill=tk.X, side=tk.TOP,expand=True)
 button = ttk.Button(text="Step One", master = frameMotor2, command = motor2.calibrate_step1)
